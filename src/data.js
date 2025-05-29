482f54744903eb05be6427839350555b793c40f2
ZCardonaZ <santiagocardonan0625@gmail.com>
2025-05-29 12:13:37 -0500

    xd

--- src/data.js
+++ src/data.js
@@ -2,47 +2,54 @@ export const productos = [
     {
       id: 1,
       nombre: "Goku para escritorio",
-      precio: 500,
+      precio: 50000,
       modelo: "/model/goku.glb",
     },
     {
       id: 2,
-      nombre: "Escritorio",
-      precio: 500000,
-      modelo: "/model/desk.glb",
+      nombre: "Wall-E Estatua",
+      precio: 985405005,
+      modelo: "/model/juguete.glb",
     },
     {
       id: 3,
-      nombre: "Lámpara de piso",
-      precio: 120000,
-      modelo: "/model/lamp.glb",
+      nombre: "Cama basica,
+      precio: 360000,
+      modelo: "/model/cama.glb",
     },
     {
         id: 4,
-        nombre: "Puerta de Madera",
-        precio: 80000,
-        modelo: "/model/door.glb",
+        nombre: "Limpiador Electrico",
+        precio: 200000,
+        modelo: "/model/electro.glb",
       },
 
        {
       id: 5,
-      nombre: "Lámpara de piso",
-      precio: 120000,
-      modelo: "/model/lamp.glb",
+      nombre: "Vasija Azteca",
+      precio: 120000000,
+      modelo: "/model/vasi.glb",
     },
 
      {
       id: 6,
-      nombre: "Lámpara de piso",
-      precio: 120000,
-      modelo: "/model/lamp.glb",
+      nombre: "PC para GTA VI",
+      precio: 100000000,
+      modelo: "/model/pc.glb",
     },
 
      {
       id: 7,
-      nombre: "Lámpara de piso",
+      nombre: "Mesa",
       precio: 120000,
-      modelo: "/model/lamp.glb",
+      modelo: "/model/mesa.glb",
+    },
+    {
+      id: 8,
+      nombre: "table",
+      precio: 10000,
+      modelo: "/model/table.glb",
     },
   ];
+
   
